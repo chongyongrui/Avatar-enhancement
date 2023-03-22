8f1726d4314d2094195812e18cfe3f24f5ba3468
MandSFun <MandSintern@gmail.com>
2023-03-22 19:56:00 +0800

    fixes

--- Avatar/Assets/Office/Scripts/ThirdPersonCam.cs
+++ Avatar/Assets/Office/Scripts/ThirdPersonCam.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
-public class ThirdPersonCam : MonoBehaviour
+using Cinemachine;
+public class ThirdPersonCam :  NetworkBehaviour
 {
     public float rotationSpeed;
     public Transform player;
@@ -9,37 +11,43 @@ public class ThirdPersonCam : MonoBehaviour
     private Quaternion freeRotation;
     private Vector3 targetDirection;
     public Transform orientation;
-
+    private CinemachineFreeLook cineMachine;
     private Transform cameraTransform;
+
+    [SerializeField] private GameObject cineMachineFollow;
 
     void Start()
     {
         cameraTransform = Camera.main.transform;
-        Cursor.lockState = CursorLockMode.Locked;
+        NetworkObject playernetwork = NetworkManager.LocalClient.PlayerObject;
+        cineMachine.LookAt = playernetwork.transform;
+        
     }
-
+    private void Awake(){
+            if(cineMachine ==null){
+                cineMachine = GetComponent<CinemachineFreeLook>();
+                
+            }
+    }
     void LateUpdate()
-{
-    input.x = Input.GetAxis("Horizontal");
-    input.y = Input.GetAxis("Vertical");
-
-    Vector3 offset = new Vector3(transform.position.x, player.position.y, transform.position.z);
-    Vector3 view = player.position - offset;
-    orientation.forward = view.normalized;
-
-    UpdateTargetDirection();
-
-    if (targetDirection != Vector3.zero)
     {
-        Vector3 lookDirection = targetDirection.normalized;
-        freeRotation = Quaternion.LookRotation(lookDirection, Vector3.up);
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, freeRotation, rotationSpeed * Time.deltaTime);
+        input.x = Input.GetAxis("Horizontal");
+        input.y = Input.GetAxis("Vertical");
 
-        // Set player rotation to match ThirdPersonCam rotation
-        player.rotation = transform.rotation;
+        Vector3 offset = new Vector3(transform.position.x, player.position.y, transform.position.z);
+        Vector3 view = player.position - offset;
+        orientation.forward = view.normalized;
+
+        UpdateTargetDirection();
+
+        if (targetDirection != Vector3.zero)
+        {
+            Vector3 lookDirection = targetDirection.normalized;
+            freeRotation = Quaternion.LookRotation(lookDirection, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, freeRotation, rotationSpeed * Time.deltaTime);
+            player.rotation = transform.rotation;
+        }
     }
-}
-
 
     public void UpdateTargetDirection()
     {
@@ -50,4 +58,13 @@ public class ThirdPersonCam : MonoBehaviour
 
         targetDirection = input.x * right + input.y * forward;
     }
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+        if(IsClient && IsOwner){
+            cineMachine.Follow =transform;
+
+        }
+    }
+    
 }
