198e2ca28c3693830fc4cdd83376dbf577655a0e
lucas koh <MandSintern@gmail.com>
2023-05-17 23:49:20 +0800

    Car

--- Avatar/Assets/Car/Scripts/GettingInAndOutCar.cs
+++ Avatar/Assets/Car/Scripts/GettingInAndOutCar.cs
@@ -3,26 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using StarterAssets;
 using Unity.Netcode;
+using Cinemachine;
 public class GettingInAndOutCar : NetworkBehaviour
 {
     public ModelSpawner modelSpawner;
     public float interactionDistance = 3f;
     public KeyCode interactKey = KeyCode.E;
     public PrometeoCarController carController;
-    private ModelSpawner modelspawner;
-    private GameObject interactingPlayer;
-     private ThirdPersonController interactingPlayerController;
-    private bool isInsideCar = false;
-    private void Start()
-    {   if(IsOwner){
-        interactingPlayer = GameObject.FindGameObjectWithTag("Player");
-        if(interactingPlayer != null){
-            interactingPlayerController  = interactingPlayer.GetComponent<ThirdPersonController>();
-            
-        }
-        carController.enabled = false;
-    }
+    public Transform carCameraPosition; // Reference to the camera position inside the car
 
+    private GameObject interactingPlayer;
+    private ThirdPersonController interactingPlayerController;
+    private bool isInsideCar = false;
+    private Transform previousParent; // Store the previous parent of the player for camera adjustment
+    private CinemachineVirtualCamera playerCamera; // Reference to the player's Cinemachine virtual camera
+
+    private void Start()
+    {
+        if (IsOwner)
+        {
+            interactingPlayer = GameObject.FindGameObjectWithTag("Player");
+            interactingPlayerController = interactingPlayer?.GetComponent<ThirdPersonController>();
+            carController.enabled = false;
+
+            // Get the Cinemachine virtual camera component from the player
+            playerCamera = interactingPlayer.GetComponentInChildren<CinemachineVirtualCamera>();
+        }
     }
     private void Update()
     {
@@ -46,32 +52,46 @@ public class GettingInAndOutCar : NetworkBehaviour
     }
 [ServerRpc(RequireOwnership = false)]
     private void GetInCarServerRPC()
-    {
-        isInsideCar = true;
+    {isInsideCar = true;
         interactingPlayerController.enabled = false;
         carController.enabled = true;
 
-//        carController = modelSpawner.spawnedModel.GetComponent<PrometeoCarController>();
+        // Store the previous parent of the player
+        previousParent = interactingPlayer.transform.parent;
 
+        // Set the player's parent to the car
         interactingPlayer.transform.SetParent(transform);
-        interactingPlayer.transform.position=  new Vector3(0,0,0);  
         interactingPlayer.transform.localPosition = Vector3.zero;
         interactingPlayer.transform.localRotation = Quaternion.identity;
         interactingPlayer.SetActive(false);
+
+        // Adjust the player's camera follow settings to face the front of the car
+        playerCamera.LookAt = transform;
+        playerCamera.Follow = carCameraPosition;
     }
 [ServerRpc(RequireOwnership = false)]
-    private void GetOutOfCarServerRPC()
-    {
-        isInsideCar = false;
-        interactingPlayerController.enabled = true;
-        carController.enabled = false;
-        carController.carSpeed= 0f;
-        
-        interactingPlayer.transform.SetParent(null);
-        interactingPlayer.transform.position = transform.position + transform.forward * 2f;
-        interactingPlayer.SetActive(true);
-        TestServerRpc();
-    }
+private void GetOutOfCarServerRPC()
+{
+    isInsideCar = false;
+    interactingPlayerController.enabled = true;
+    carController.enabled = false;
+    carController.carSpeed = 0f;
+
+    // Reset the player's parent to the previous parent
+    interactingPlayer.transform.SetParent(previousParent);
+
+    // Reset the player's camera follow settings to default
+    playerCamera.LookAt = interactingPlayer.transform;
+    playerCamera.Follow = interactingPlayer.transform;
+
+    interactingPlayer.transform.position = transform.position + transform.forward * 2f;
+    interactingPlayer.SetActive(true);
+
+    // Stop the car from moving
+    Rigidbody carRigidbody = carController.GetComponent<Rigidbody>();
+    carRigidbody.velocity = Vector3.zero;
+}
+
 
     private void OnTriggerEnter(Collider other)
     {
