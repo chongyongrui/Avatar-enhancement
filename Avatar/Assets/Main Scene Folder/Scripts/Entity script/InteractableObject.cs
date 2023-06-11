6b7ad940c55d6f34d556c31f83eefaad9dec9235
lucas koh <MandSintern@gmail.com>
2023-06-11 23:20:16 +0800

    Added working animations and script

--- Avatar/Assets/Main Scene Folder/Scripts/Entity script/InteractableObject.cs
+++ Avatar/Assets/Main Scene Folder/Scripts/Entity script/InteractableObject.cs
@@ -3,34 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Animations.Rigging;
 public class InteractableObject : MonoBehaviour
-{ public bool CanBeInteracted { get; private set; } = true; // Can the object be interacted with?
-    public string grabTrigger = "Grabitem"; // The name of the grab animation trigger
-    public void Interact(Transform handIKTarget, Animator animator)
+{ 
+    
+    [SerializeField] private string weaponIdentifier;
+
+    // Other methods and variables...
+
+    public string GetWeaponIdentifier()
     {
-        // Perform object-specific interaction logic here
-        Debug.Log("Interacting with object: " + gameObject.name);
-
-        // Play the grab animation
-       
-
-        // Set the IK target position
-       // SetIKTargetPosition(handIKTarget.position);
-
-    handIKTarget.position = transform.position;
-        // Example interaction: Parent the object to the character's hand
-
-        
-        //transform.SetParent(handIKTarget);
-
-        // transform.localPosition = Vector3.zero;
-        // transform.localRotation = Quaternion.identity;
- animator.SetTrigger(grabTrigger);
-        CanBeInteracted = false; // Disable further interaction with this object
+        return weaponIdentifier;
     }
 
-    private void SetIKTargetPosition(Vector3 position)
-    {
-        // Set the IK target position
-        AnimatorIKManager.SetIKTargetPosition(position);
-    }
 }
--- Avatar/Assets/Main Scene Folder/Scripts/Test/PlayerInteractable.cs
+++ Avatar/Assets/Main Scene Folder/Scripts/Test/PlayerInteractable.cs
@@ -1,46 +1,156 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Animations.Rigging;
-
 public class PlayerInteractable : MonoBehaviour
 {
-   public float interactionRadius = 2f; // The radius for detecting nearby objects
-    public KeyCode interactionKey = KeyCode.E; // The key to trigger the interaction
-    public Transform handIKTarget; // The IK target for the character's hand
-    private Animator animator; // Reference to the animator component
-    
-    private RigBuilder rb;
- 
-    private void Start(){
-        animator = GetComponent<Animator>();
-           rb = GetComponentInChildren<RigBuilder>();
-        rb.enabled = false;
+    public static PlayerInteractable Instance { get; private set; }
+
+    [SerializeField] private float interactionRange = 2f;
+    private Animator anim;
+    private int animPickup;
+    private Transform currentInteractable;
+    private bool isPickupAnimationPlaying;
+
+    [SerializeField] private Transform handBone; // Reference to the hand bone GameObject;
+    [SerializeField] private Transform weaponPlaceholder; // Empty GameObject acting as a placeholder for the weapon;
+
+    // Dictionary to map weapon identifiers to their corresponding prefabs
+    [SerializeField] private Dictionary<string, GameObject> weaponPrefabs;
+
+    [SerializeField] private GameObject AK47prefab;
+    [SerializeField] private float delayBeforeSpawn;
+
+    [SerializeField] private float xOffset;
+    [SerializeField] private float yOffset;
+    [SerializeField] private float zOffset;
+    [SerializeField] private float xRotation;
+    [SerializeField] private float yRotation;
+    [SerializeField] private float zRotation;
+    private bool hasWeapon;
+
+    private void Start()
+    {
+        weaponPrefabs = new Dictionary<string, GameObject>();
+        weaponPrefabs["AK47"] = AK47prefab;
+        anim = GetComponent<Animator>();
+        AssignAnimationIDs();
     }
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    private void AssignAnimationIDs()
+    {
+        animPickup = Animator.StringToHash("Pickup");
+    }
+
     private void Update()
     {
-        if (Input.GetKeyDown(interactionKey))
-        {   
-            Collider[] colliders = Physics.OverlapSphere(transform.position, interactionRadius);
-
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Collider[] colliders = Physics.OverlapSphere(transform.position, interactionRange);
             foreach (Collider collider in colliders)
             {
                 if (collider.CompareTag("Interactable"))
-                {   rb.enabled = true;
-                    InteractableObject interactableObject = collider.GetComponent<InteractableObject>();
-                    if (interactableObject != null && interactableObject.CanBeInteracted)
+                {
+                    InteractableObject objectInteraction = collider.GetComponent<InteractableObject>();
+                    if (objectInteraction != null)
                     {
-                        interactableObject.Interact(handIKTarget, animator);
-                        break; // Interact with only one object at a time (remove this line if you want to interact with multiple objects simultaneously)
+                        // Face the object
+                        Vector3 lookDirection = collider.transform.position - transform.position;
+                        lookDirection.y = 0f;
+                        transform.rotation = Quaternion.LookRotation(lookDirection);
+
+                        // Trigger the object's interaction
+
+
+                        // Get the identifier of the weapon
+                        string weaponIdentifier = objectInteraction.GetWeaponIdentifier();
+
+                        // Trigger the pickup animation with the weapon identifier
+                        TriggerPickupAnimation(collider.transform.position, weaponIdentifier);
+
+                        Debug.Log("Playing");
+                        break;
                     }
                 }
             }
         }
     }
 
-    private void OnDrawGizmosSelected()
+    public void SetCurrentInteractable(Transform interactable)
     {
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, interactionRadius);
+        currentInteractable = interactable;
     }
+
+    public void TriggerPickupAnimation(Vector3 objectPosition, string weaponIdentifier)
+    {
+        // Face the object
+        Vector3 lookDirection = objectPosition - transform.position;
+        lookDirection.y = 0f;
+        transform.rotation = Quaternion.LookRotation(lookDirection);
+
+        // Trigger the pickup animation
+        anim.SetTrigger(animPickup);
+        isPickupAnimationPlaying = true;
+
+        // Check if the weapon identifier exists in the dictionary
+        if (weaponPrefabs.ContainsKey(weaponIdentifier))
+        {
+            // Get the corresponding weapon prefab
+            GameObject weaponPrefab = weaponPrefabs[weaponIdentifier];
+
+            // Call the SpawnWeapon method on the next frame with the correct prefab
+            StartCoroutine(DelayedSpawnWeapon(weaponPrefab));
+        }
+        else
+        {
+            Debug.LogWarning("No prefab found for weapon identifier: " + weaponIdentifier);
+        }
+    }
+
+    private IEnumerator DelayedSpawnWeapon(GameObject weaponPrefab)
+    {
+        //Delay before instantiating the weapon into the hand;
+        yield return new WaitForSeconds(delayBeforeSpawn);
+
+        // Spawn the weapon
+        if (weaponPrefab != null)
+        {
+            // Instantiate the weapon prefab
+            GameObject weapon = Instantiate(weaponPrefab, handBone);
+
+            // Set the weapon's position and rotation based on the placeholder
+            weapon.transform.position = weaponPlaceholder.position;
+            weapon.transform.rotation = weaponPlaceholder.rotation;
+             weapon.transform.localPosition = new Vector3(xOffset, yOffset, zOffset);
+        weapon.transform.localRotation = Quaternion.Euler(xRotation, yRotation, zRotation);
+        hasWeapon = true;
+        SetHasWeaponTrue();
+
+        }
+    }
+
+
+    // Called from the animation timeline when the weapon placeholder needs to be set
+    public void SetWeaponPlaceholder(Transform placeholder)
+    {
+        weaponPlaceholder = placeholder;
+    }
+    public void SetHasWeaponTrue()
+    {
+        anim.SetBool("HasWeapon", true);
+    }
+
+    // Called from the animation timeline to set the HasWeapon parameter to false
+    public void SetHasWeaponFalse()
+    {
+        anim.SetBool("HasWeapon", false);
+    }
+
 }
