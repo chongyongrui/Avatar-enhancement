c42ab635f34cb67717784076c6a0b7a0c19418e5
Aortz <65661817+Aortz@users.noreply.github.com>
2023-07-04 15:42:34 +0800

    Authentication (#10)
    * added git attribute
    
    * fix ress file type
    
    * modified login logic to send to aries agent
    
    * edited authentication logic for adding user
    
    * edited testingNetwork Manager
    
    * added loading screen
    
    * added background assets
    
    * added background assets
    
    * changes: updated interface for registration page
    
    * edited c# to include function to build dockerfile
    
    * edited script
    
    * edited loading and registration screens
    
    * stuff
    
    * changes to the registration page
    
    * added docker compose yaml
    
    * edited docker compose
    
    * edited docker-compose file
    
    * edited help text for blockchain roles
    
    * added scenes in unity
    
    * added in fonts used for scenes
    
    * resolved pull merge conflicts
    
    * fixed merge conflict
    
    * modified login logic to send to aries agent
    
    * edited testingNetwork Manager
    
    * added git attribute
    
    * fix ress file type
    
    * edited authentication logic for adding user
    
    * added loading screen
    
    * added background assets
    
    * added background assets
    
    * changes: updated interface for registration page
    
    * edited c# to include function to build dockerfile
    
    * edited script
    
    * edited loading and registration screens
    
    * resolved merge conflict
    
    * changes to the registration page
    
    * added docker compose yaml
    
    * edited docker compose
    
    * edited docker-compose file
    
    * edited help text for blockchain roles
    
    * added scenes in unity
    
    * added in fonts used for scenes
    
    * fixed merge conflict
    
    * fixed merge conflict
    
    ---------
    
    Co-authored-by: MandSFun <MandSintern@gmail.com>

---
+++ Avatar/Assets/Main Scene Folder/Scripts/Network Manage/loadOnClick.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Text;
+using TMPro;
+using Newtonsoft.Json;
+
+
+public class loadOnClick : MonoBehaviour
+{ 
+    public GameObject blackOutSquare;
+    public void Update(){
+        if(Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Space)){
+            StartCoroutine(FadeBlackOutSquare());
+        }
+    }
+
+    public IEnumerator FadeBlackOutSquare(bool fadeToBlack = true, int fadeSpeed = 1){
+        Color objectColor = blackOutSquare.GetComponent<Image>().color;
+        float fadeAmount;
+        if (fadeToBlack){
+            while (blackOutSquare.GetComponent<Image>().color.a < 1){
+                fadeAmount = objectColor.a + (fadeSpeed * Time.deltaTime);
+                objectColor = new Color(objectColor.r, objectColor. g,objectColor.b, fadeAmount);
+                blackOutSquare.GetComponent<Image>().color = objectColor;
+                yield return null;
+            }
+            Loader.Load(Loader.Scene.Registration);
+        }
+
+    }
+    
+
+}
