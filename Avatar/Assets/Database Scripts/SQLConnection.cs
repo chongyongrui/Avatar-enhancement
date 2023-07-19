a6ca212c6843d062eca9ec5295fdf67623d64401
chongyongrui <88470129+chongyongrui@users.noreply.github.com>
2023-07-19 17:31:20 +0800

    refactor, update files

--- Avatar/Assets/Database Scripts/SQLConnection.cs
+++ Avatar/Assets/Database Scripts/SQLConnection.cs
@@ -3,51 +3,88 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using UnityEngine;
+using System.Data.SqlClient;
+using System.Configuration;
 
 public class SQLConnection : MonoBehaviour
 {
-    class Program
-    {
-        static void Main(string[] args)
+   void Start()
         {
-            try
+
+        //DisplayWeapons();
+
+
+        //   SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+
+        //builder.DataSource = "<your_server.database.windows.net>"; 
+        //builder.UserID = "";            
+        //builder.Password = "<your_password>";     
+        //builder.InitialCatalog = "<your_database>";
+        //builder.ConnectionString = "data source=DESKTOP-2P23NMB;initial catalog=AvatarProject;trusted_connection=true";
+        /*
+        System.Data.SqlClient.SqlConnectionStringBuilder builder = new System.Data.SqlClient.SqlConnectionStringBuilder();
+        builder["Data Source"] = "DESKTOP-2P23NMB";
+        builder["integrated Security"] = true;
+        builder["Initial Catalog"] = "AvatarProject";
+        Console.WriteLine(builder.ConnectionString);
+        */
+        /*
+        SqlConnection conn = new SqlConnection();
+        conn.ConnectionString =
+          "Data Source=DESKTOP-2P23NMB;" +
+          "Initial Catalog=AvatarProject;" +
+          "User id=DESKTOP-2P23NMB\\userAdmin;" +
+          "Password=;";
+        conn.Open();
+        */
+        //string connectionString = "data source=DESKTOP-2P23NMB;initial catalog=AvatarProject;Integrated Security=true";
+         //SqlConnection con = new SqlConnection(builder.ConnectionString);
+         SqlConnection conn = new SqlConnection("data source =.;initial catalog=master;integrated security=true;");
+        conn.Open();
+        string query = "SELECT * FROM  weaponInventoryList";
+        SqlCommand sqlCommand = new SqlCommand(query, conn);
+        SqlDataReader reader = sqlCommand.ExecuteReader();
+        while (reader.Read())
+        {
+            string output = "Output playerID = " + reader.GetValue(0) + " and weaponID = " + reader.GetValue(1);
+            Debug.Log(output);
+        }
+
+
+
+        }
+
+
+    public void DisplayWeapons()
+    {
+       // "Data Source=DESKTOP-2P23NMB;Initial Catalog=AvatarProject;User ID=DESKTOP-2P23NMB\\userAdmin;Password=";
+        string connectionString = "Data Source=DESKTOP-2P23NMB;Initial Catalog=AvatarProject;Integrated Security=true";
+       //string connectionString = "Data Source=DESKTOP-2P23NMB;Initial Catalog=AvatarProject;User ID=DESKTOP-2P23NMB\\userAdmin;Password=";
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+
+            connection.Open();
+
+
+            using (var command = connection.CreateCommand())
             {
-                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
 
-                //builder.DataSource = "<your_server.database.windows.net>"; 
-                //builder.UserID = "";            
-                //builder.Password = "<your_password>";     
-                //builder.InitialCatalog = "<your_database>";
-                builder.ConnectionString = "data source=DESKTOP-2P23NMB;initial catalog=AvatarProject;trusted_connection=true";
+                command.CommandText = "SELECT * FROM weaponInventoryList;";
 
-
-                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                using (System.Data.IDataReader reader = command.ExecuteReader())
                 {
-                    Console.WriteLine("\nQuery data example:");
-                    Console.WriteLine("=========================================\n");
-
-                    connection.Open();
-
-                    String sql = "SELECT * FROM weaponInventoryList";
-
-                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    while (reader.Read())
                     {
-                        using (SqlDataReader reader = command.ExecuteReader())
-                        {
-                            while (reader.Read())
-                            {
-                                Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
-                            }
-                        }
+                        Debug.Log("Player ID: " + reader["playerID"] + " \tWeapon ID: " + reader["weaponID"] );
                     }
+                    reader.Close();
                 }
             }
-            catch (SqlException e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-            Console.WriteLine("\nDone. Press enter.");
-            Console.ReadLine();
+
+            connection.Close();
+
+
         }
     }
 }
+
