ebb66644e61d32bbd23437193efc8af67cfd0df2
LurkingGoo <100992221+LurkingGoo@users.noreply.github.com>
2023-08-22 10:51:45 +0800

    Chat system integration (#34)
    * Tidied up files
    
    * Bug fixes, fixed error on spawn with camera and removed redundant dependencies
    
    * Changed readme and improved script
    
    * Finished looping of car upon button click
    
    * FIxed camera bug
    
    * Readme file changed
    
    * Changed model prefab and tidied script
    
    * NPC addition
    
    * NPC adjustment of rig
    
    * Finish of npc rigging, added import instructions in readme
    
    * Bug fixes of camera and gun script.
    
    * Further bug fixes
    
    * Addition of backpack and pistol
    
    * Bug fixes and addition of head look+ npc interaction
    
    * Addition of blood pool, bug fixes
    
    * Solved merge error
    
    * Solved merge conflict
    
    * Bug fixes with camera
    fixing of player spawn in multiplayer,
    fixing of exe file
    
    * Removal and changes to
    #	Avatar/Assets/Scenes/Main.unity
    #	Avatar/UserSettings/Layouts/default-2022.dwlt
    #	Avatar/databasefiles.db
    
    * Changed camera priority
    
    * Fixed camera bugs, reenabled fps and fixed fovs
    
    * Create LICENSE
    
    * Addition of chat system
    
    * Addition of chat system, changed login and registration
    
    * Bug fixes
    
    * Fixed merge conflict
    
    * added in ip features to networkmanager
    
    * shooting game fix
    
    * Chat system integration
    
    * fixes
    
    * deletion of files
    
    * Chat system integration
    
    * the purge
    
    * git ignore revert
    
    ---------
    
    Co-authored-by: MandSFun <MandSintern@gmail.com>

--- Avatar/Assets/Main Scene Folder/Chat system/Scripts/ChatManager.cs
+++ Avatar/Assets/Main Scene Folder/Chat system/Scripts/ChatManager.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
 using TMPro;
+using StarterAssets;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class ChatManager : NetworkBehaviour
 {
@@ -11,54 +14,103 @@ public class ChatManager : NetworkBehaviour
     [SerializeField] ChatMessage chatMessagePrefab;
     [SerializeField] CanvasGroup chatContent;
     [SerializeField] TMP_InputField chatInput;
+    [SerializeField] GameObject holder;
 
+    public PlayerInput interactingPlayerController;
+    public Button chatButton;
+    public GameObject player;
+    bool chatStatus = false;
+    bool playerspawned = false;
+
+
+
+
+    public void Start()
+    {
+
+    }
     public override void OnNetworkSpawn()
     {
         base.OnNetworkSpawn();
-        string message ="Player "+NetworkManager.Singleton.LocalClientId+" has joined.";
-        SendChatMessageServerRpc(message,NetworkManager.Singleton.LocalClientId);
-        
+        player = GameObject.FindGameObjectWithTag("Player");
+        interactingPlayerController = player.GetComponent<PlayerInput>();
+        string message = "Player " + NetworkManager.Singleton.LocalClientId + " has joined.";
+        SendChatMessageServerRpc(message, NetworkManager.Singleton.LocalClientId);
+        playerspawned = true;
 
     }
     public override void OnNetworkDespawn()
     {
         base.OnNetworkDespawn();
-        string message ="Player "+NetworkManager.Singleton.LocalClientId+" has left.";
-        SendChatMessageServerRpc(message,NetworkManager.Singleton.LocalClientId);
+        string message = "Player " + NetworkManager.Singleton.LocalClientId + " has left.";
+        SendChatMessageServerRpc(message, NetworkManager.Singleton.LocalClientId);
     }
-    void Update() 
+    void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Return))
+        if (Input.GetKeyDown(KeyCode.Return))
         {
-            SendChatMessage(chatInput.text );
+            SendChatMessage(chatInput.text);
             chatInput.text = "";
         }
+        if (playerspawned)
+        {
+            if (chatStatus)
+            {
+                interactingPlayerController.enabled = false;
+                Debug.Log("Set false");
+            }
+            else
+            {
+                interactingPlayerController.enabled = true;
+                Debug.Log("set true");
+            }
+        }
+
     }
 
     public void SendChatMessage(string _message)
-    { 
-        if(string.IsNullOrWhiteSpace(_message)) return;
+    {
+        if (string.IsNullOrWhiteSpace(_message)) return;
 
         //string S = "Player " + NetworkManager.Singleton.LocalClientId + " > " +  _message;
-        SendChatMessageServerRpc(_message,NetworkManager.Singleton.LocalClientId); 
+        SendChatMessageServerRpc(_message, NetworkManager.Singleton.LocalClientId);
     }
-   
-    void AddMessage(string msg,ulong senderID)
+    public void Chatpop()
+    {
+        if (holder.activeSelf)
+        {
+            chatStatus = false;
+            holder.SetActive(false);
+
+            //     interactingPlayerController = player.GetComponent<ThirdPersonController>();
+            //interactingPlayerController.enabled = true;
+            Debug.Log("Player input set fasle");
+
+        }
+        else
+        {
+            holder.SetActive(true);
+            chatStatus = true;
+        }
+        //   interactingPlayerController.enabled = false;
+
+    }
+    void AddMessage(string msg, ulong senderID)
     {
         ChatMessage CM = Instantiate(chatMessagePrefab, chatContent.transform);
-        CM.SetMessage(senderID,msg);
+        CM.SetMessage(senderID, msg);
     }
 
     [ServerRpc(RequireOwnership = false)]
-    void SendChatMessageServerRpc(string message,ulong senderID)
+    void SendChatMessageServerRpc(string message, ulong senderID)
     {
-        ReceiveChatMessageClientRpc(message,senderID);
+        ReceiveChatMessageClientRpc(message, senderID);
     }
 
     [ClientRpc]
-    void ReceiveChatMessageClientRpc(string message,ulong senderID)
+    void ReceiveChatMessageClientRpc(string message, ulong senderID)
     {
         //ChatManager.Singleton.AddMessage(message,senderID);
-        AddMessage(message,senderID);
+        AddMessage(message, senderID);
     }
 }
