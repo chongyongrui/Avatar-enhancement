cc6a61d9a4fd9358616b938692df633f29ff53da
chongyongrui <yrchong2000@gmail.com>
2023-08-26 10:58:57 +0800

    add crouching feature for minigame

--- Avatar/Assets/Main Scene Folder/Scripts/Unused Scripts/CameraFirstPerson.cs
+++ Avatar/Assets/Main Scene Folder/Scripts/Unused Scripts/CameraFirstPerson.cs
@@ -17,56 +17,87 @@ public class CameraFirstPerson : MonoBehaviour
     public float defaultFOV = 56f;
     public float zoomedFOV = 30f;
     public bool isZoomed = false;
+    public bool isCrouching = false;
     [SerializeField] float movementSpeed = 2f;
+    private Vector3 oldAngles;
+    private bool revertedPosition = true;
+    [SerializeField] GameObject hitBox;
     void Start()
     {
         
         Cursor.lockState = CursorLockMode.Locked;
+        
     }
 
     void Update()
     {
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.position = transform.position + transform.forward * Time.deltaTime * movementSpeed;
-        }
-        else if (Input.GetKey(KeyCode.S)) 
-        {
-            transform.position = transform.position - transform.forward * Time.deltaTime * movementSpeed;
-        }else if (Input.GetKey(KeyCode.D))
-        {
-            transform.position = transform.position + transform.right * Time.deltaTime * movementSpeed;
-        }else if (Input.GetKey(KeyCode.A))
-        {
-            transform.position = transform.position - transform.right * Time.deltaTime * movementSpeed;
+        if (Input.GetKeyDown(KeyCode.Space))
+            {
+            isCrouching = !isCrouching;
         }
 
-
-        if (Input.GetKeyDown(KeyCode.Mouse1))
+        if (isCrouching)
         {
-            isZoomed = !isZoomed;
-        }
+            revertedPosition = false;
+            transform.eulerAngles = new Vector3(
+                 -20, -180, 0);
+            hitBox.GetComponent<Collider>().enabled = false;
 
-        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
-
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-
-        yRotation += mouseX;
-        yRotation = Mathf.Clamp(90f, yRotation, 90f);
-
-        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
-        //playerBody.Rotate(Vector3.up * mouseX);
-        Camera cam = this.GetComponent<Camera>();
-        if (isZoomed)
-        {
-            cam.fieldOfView = zoomedFOV;
         }
         else
         {
-            cam.fieldOfView = defaultFOV;
+            
+            if (!revertedPosition)
+            {
+                transform.eulerAngles = oldAngles;
+                revertedPosition = true;
+                hitBox.GetComponent<Collider>().enabled = true;
+            }
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.position = transform.position + transform.forward * Time.deltaTime * movementSpeed;
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                transform.position = transform.position - transform.forward * Time.deltaTime * movementSpeed;
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                transform.position = transform.position + transform.right * Time.deltaTime * movementSpeed;
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                transform.position = transform.position - transform.right * Time.deltaTime * movementSpeed;
+            }
+            
+
+
+            if (Input.GetKeyDown(KeyCode.Mouse1))
+            {
+                isZoomed = !isZoomed;
+            }
+
+            float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+            yRotation += mouseX;
+            yRotation = Mathf.Clamp(90f, yRotation, 90f);
+
+            transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+            //playerBody.Rotate(Vector3.up * mouseX);
+            Camera cam = this.GetComponent<Camera>();
+            if (isZoomed)
+            {
+                cam.fieldOfView = zoomedFOV;
+            }
+            else
+            {
+                cam.fieldOfView = defaultFOV;
+            }
         }
 
     }
